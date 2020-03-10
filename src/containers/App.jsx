db6a309d98b39dffbac0ae98b27e72bdfddff614
Diego Alejandro Osorio <42728227+Diegoalesco95@users.noreply.github.com>
2020-03-10 09:25:14 -0500

    Estructura y Estilos del Footer

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -4,6 +4,7 @@ import Search from '../components/Search';
 import Categories from '../components/Categories';
 import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
+import Footer from '../components/Footer';
 
 import '../assets/styles/App.scss';
 
@@ -22,6 +23,7 @@ const App = () => {
           <CarouselItem />
         </Carousel>
       </Categories>
+      <Footer />
     </div>
   );
 };
