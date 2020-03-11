ad4542bde5fdc0d20b43108ec1c932aa2823ee8b
Diego Alejandro Osorio <42728227+Diegoalesco95@users.noreply.github.com>
2020-03-11 15:10:07 -0500

    Sign up

--- src/containers/Signup.jsx
+++ src/containers/Signup.jsx
@@ -1,72 +1,96 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import googleIcon from '../assets/static/google-icon.png';
 import twitterIcon from '../assets/static/twitter-icon.png';
 
 import '../assets/styles/App.scss';
 
-const Signup = () => (
-  <section className='register'>
-    <section className='register__container'>
-      <h2>Regístrate</h2>
-      <form className='register__container--form'>
-        <input
-          aria-label='Nombre'
-          type='text'
-          name='Nombre'
-          id='name'
-          className='input--signup'
-          placeholder='Nombre'
-        />
-        <input
-          aria-label='Correo'
-          type='email'
-          name='Correo'
-          id='email'
-          className='input--signup'
-          placeholder='Correo'
-        />
-        <input
-          aria-label='Contraseña'
-          type='password'
-          name='Contraseña'
-          id='password'
-          className='input--signup'
-          placeholder='Contraseña'
-        />
-        <input
-          aria-label='Confirmar contraseña'
-          type='password'
-          name='Confirmar contraseña'
-          id='confirm-password'
-          className='input--signup'
-          placeholder='Confirmar contraseña'
-        />
-        <button type='button' className='button'>
-          Registrarme
-        </button>
-      </form>
-      <section className='register__container--social-media'>
-        <div className='google'>
-          <img className='google__img' src={googleIcon} alt='Google' />
-          <p>
-            <a href='/'>Registro con Google </a>
-          </p>
-        </div>
-        <div className='twitter'>
-          <img className='twitter__img' src={twitterIcon} alt='Twitter' />
-          <p>
-            <a href='/'>Registro con Twitter</a>
-          </p>
-        </div>
+const Signup = (props) => {
+  const [form, setValues] = useState({
+    email: '',
+    name: '',
+    password: '',
+  });
+
+  const handleInput = (event) => {
+    setValues({
+      ...form,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(form);
+  };
+
+  return (
+    <section className='register'>
+      <section className='register__container'>
+        <h2>Regístrate</h2>
+        <form className='register__container--form' onSubmit={handleSubmit}>
+          <input
+            name='name'
+            aria-label='Nombre'
+            type='text'
+            id='name'
+            className='input--signup'
+            placeholder='Nombre'
+            onChange={handleInput}
+          />
+          <input
+            name='email'
+            aria-label='Correo'
+            type='email'
+            id='email'
+            className='input--signup'
+            placeholder='Correo'
+            onChange={handleInput}
+          />
+          <input
+            name='password'
+            aria-label='Contraseña'
+            type='password'
+            id='password'
+            className='input--signup'
+            placeholder='Contraseña'
+            onChange={handleInput}
+          />
+          <input
+            aria-label='Confirmar contraseña'
+            type='password'
+            name='password'
+            id='confirm-password'
+            className='input--signup'
+            placeholder='Confirmar contraseña'
+            onChange={handleInput}
+          />
+          <button type='submit' className='button'>
+            Registrarme
+          </button>
+        </form>
+        <section className='register__container--social-media'>
+          <div className='google'>
+            <img className='google__img' src={googleIcon} alt='Google' />
+            <p>
+              <a href='/'>Registro con Google </a>
+            </p>
+          </div>
+          <div className='twitter'>
+            <img className='twitter__img' src={twitterIcon} alt='Twitter' />
+            <p>
+              <a href='/'>Registro con Twitter</a>
+            </p>
+          </div>
+        </section>
+        <p className='register__container--login'>
+          ¿Ya tienes una cuenta?
+          <br />
+          <Link to='/login'>Inicia sesión</Link>
+        </p>
       </section>
-      <p className='register__container--login'>
-        ¿Ya tienes una cuenta?
-        <br />
-        <Link to='/login'>Inicia sesión</Link>
-      </p>
     </section>
-  </section>
-);
+  );
+};
 
 export default Signup;
