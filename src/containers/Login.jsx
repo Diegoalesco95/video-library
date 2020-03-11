f1a6e1923c572f328fbb126f59c031187176d786
Diego Alejandro Osorio <42728227+Diegoalesco95@users.noreply.github.com>
2020-03-11 11:30:58 -0500

    Formulario de Login

--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -1,63 +1,83 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import googleIcon from '../assets/static/google-icon.png';
 import twitterIcon from '../assets/static/twitter-icon.png';
 
 import '../assets/styles/App.scss';
 
-const Login = () => (
-  <section className='login'>
-    <section className='login__container'>
-      <h2>Inicia Sesión</h2>
-      <form className='login__container--form'>
-        <input
-          aria-label='Correo'
-          type='email'
-          name='email'
-          id='email'
-          className='input--login'
-          placeholder='Correo'
-        />
-        <input
-          aria-label='Contraseña'
-          type='password'
-          name='password'
-          id='password'
-          className='input--login'
-          placeholder='Contraseña'
-        />
-        <button className='button' type='button'>
-          Iniciar Sesión
-        </button>
-        <div className='login__container--remember-me'>
-          <label htmlFor='cbox1'>
-            <input type='checkbox' id='cbox1' value='checkbox' />
-            Recuérdame
-          </label>
-          <a href='/'>Olvidé mi contraseña</a>
-        </div>
-      </form>
-      <section className='login__container--social-media'>
-        <div className='google'>
-          <img className='google__img' src={googleIcon} alt='Google' />
-          <p>
-            <a href='/'>Inicia sesión con Google</a>
-          </p>
-        </div>
-        <div className='twitter'>
-          <img className='twitter__img' src={twitterIcon} alt='Twitter' />
-          <p>
-            <a href='/'>Inicia sesión con Twitter</a>
-          </p>
-        </div>
+const Login = () => {
+  const [form, setValues] = useState({
+    email: '',
+  });
+
+  const handleInput = (event) => {
+    setValues({
+      ...form,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(form);
+  };
+
+  return (
+    <section className='login'>
+      <section className='login__container'>
+        <h2>Inicia Sesión</h2>
+        <form className='login__container--form' onSubmit={handleSubmit}>
+          <input
+            name='email'
+            aria-label='Correo'
+            type='email'
+            id='email'
+            className='input--login'
+            placeholder='Correo'
+            onChange={handleInput}
+          />
+          <input
+            name='password'
+            aria-label='Contraseña'
+            type='password'
+            id='password'
+            className='input--login'
+            placeholder='Contraseña'
+            onChange={handleInput}
+          />
+          <button className='button' type='submit'>
+            Iniciar Sesión
+          </button>
+          <div className='login__container--remember-me'>
+            <label htmlFor='cbox1'>
+              <input type='checkbox' id='cbox1' value='checkbox' />
+              Recuérdame
+            </label>
+            <a href='/'>Olvidé mi contraseña</a>
+          </div>
+        </form>
+        <section className='login__container--social-media'>
+          <div className='google'>
+            <img className='google__img' src={googleIcon} alt='Google' />
+            <p>
+              <a href='/'>Inicia sesión con Google</a>
+            </p>
+          </div>
+          <div className='twitter'>
+            <img className='twitter__img' src={twitterIcon} alt='Twitter' />
+            <p>
+              <a href='/'>Inicia sesión con Twitter</a>
+            </p>
+          </div>
+        </section>
+        <p className='login__container--register'>
+          ¿No tienes ninguna cuenta?
+          <br />
+          <Link to='/signup'>Regístrate!</Link>
+        </p>
       </section>
-      <p className='login__container--register'>
-        ¿No tienes ninguna cuenta?
-        <br />
-        <Link to='/signup'>Regístrate!</Link>
-      </p>
     </section>
-  </section>
-);
+  );
+};
 
 export default Login;
