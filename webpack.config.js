367c461253823c16fa2aab1b3ab69764ac8916af
Diegoalesco95 <42728227+Diegoalesco95@users.noreply.github.com>
2020-03-24 19:27:21 -0500

    Configuraciones de rutas

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'js/[name].[hash].js',
-    publicPath: './',
+    publicPath: 'Platzi-video/',
     chunkFilename: 'js/[id].[chunkhash].js',
   },
   optimization: {
@@ -76,7 +76,7 @@ module.exports = {
     new AddAssetHtmlPlugin({
       filepath: path.resolve(__dirname, 'dist/js/*.dll.js'),
       outputPath: 'js/',
-      publicPath: './js/',
+      publicPath: 'Platzi-video/js/',
     }),
     new CleanWebpackPlugin({
       cleanOnceBeforeBuildPatterns: ['**/app.*'],
