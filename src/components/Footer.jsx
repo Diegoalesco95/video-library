cf3f147ec2ad115af328f52abc377709ae59b667
Diegoalesco95 <42728227+Diegoalesco95@users.noreply.github.com>
2020-03-24 20:55:47 -0500

    config Links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Footer = () => (
   <footer className='footer'>
-    <a href='/'>Términos de uso</a>
-    <a href='/'>Declaración de privacidad</a>
-    <a href='/'>Centro de ayuda</a>
+    <Link to='/Platzi-video/'>Términos de uso</Link>
+    <Link to='/Platzi-video/'>Declaración de privacidad</Link>
+    <Link to='/Platzi-video/'>Centro de ayuda</Link>
   </footer>
 );
 
