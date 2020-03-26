0b5c7b10673504910b90fc1a965c2f4fb13965af
Diegoalesco95 <42728227+Diegoalesco95@users.noreply.github.com>
2020-03-26 14:41:31 -0500

    Usando Nodemon y Dotenv

--- src/server/server.js
+++ src/server/server.js
@@ -1,15 +1,22 @@
 import express from 'express';
+import config from './config';
+
+const { env, port } = config;
 
 const app = express();
+
+if (env === 'development') {
+  console.log(`Server running on mode ${env}`);
+}
 
 app.get('*', (req, res) => {
   res.send({ hello: 'express' });
 });
 
-app.listen(3001, (error) => {
+app.listen(port, (error) => {
   if (error) {
     console.log(error);
   } else {
-    console.log('Listening http://localhost:3001');
+    console.log(`Server running on http://localhost:${port}`);
   }
 });
