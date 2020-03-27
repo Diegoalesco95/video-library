1f83e3617aaa5c82e62221fbd85918c097af95e6
Diegoalesco95 <42728227+Diegoalesco95@users.noreply.github.com>
2020-03-27 09:57:23 -0500

    Abstrayendo React Router, creando history y haciendo initialState más accesible

---
+++ src/frontend/routes/serverRouter.js
@@ -0,0 +1,34 @@
+import Home from '../containers/Home';
+import Login from '../containers/Login';
+import Signup from '../containers/Signup';
+import Player from '../containers/Player';
+import NotFound from '../containers/NotFound';
+
+const routes = [
+  {
+    exact: true,
+    path: '/Platzi-video/',
+    component: Home,
+  },
+  {
+    exact: true,
+    path: '/Platzi-video/login',
+    component: Login,
+  },
+  {
+    exact: true,
+    path: '/Platzi-video/signup',
+    component: Signup,
+  },
+  {
+    exact: true,
+    path: '/Platzi-video/player/:id',
+    component: Player,
+  },
+  {
+    name: 'NotFound',
+    component: NotFound,
+  },
+];
+
+export default routes;
--- src/server/server.js
+++ src/server/server.js
@@ -23,21 +23,21 @@ if (env === 'development') {
 }
 
 app.get('*', (req, res) => {
-  res.send(`
-  <!DOCTYPE html>
-  <html lang="es">
-    <head>
-      <meta charset="UTF-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <link rel="stylesheet" href="assets/app.css" type="text/css">
-      <title>Platzi Video</title>
-    </head>
-    <body>
-      <div id="app"></div>
-      <script src="assets/app.js" type="text/javascript"></script>
-    </body>
-  </html>
-  `);
+  res.send(
+    `<!DOCTYPE html>
+<html lang="es">
+  <head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="assets/app.css"  >
+    <title>Platzi Video</title>
+  </head>
+  <body>
+    <div id="app"></div>
+    <script src="assets/app.js" type="text/javascript"></script>
+  </body>
+</html>`
+  );
 });
 
 app.listen(port, (error) => {
